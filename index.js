b90ec2dcbc4afd0cc6dc13b7ff41f80e7cef929c
Daniel Sinkpon <danielsinkpon@gmail.com>
2025-04-30 07:45:47 -0400

    Randomize color trail left by the hover effect
    Create trail functionality where the color of a
    cell will change on hover and will maintain that
    color a few milliseconds before turning white.
    
    Randomize the color for each new cell that the
    user hovers with the mouse.

--- index.js
+++ index.js
@@ -3,10 +3,22 @@ const setGridButton = document.querySelector("#set-grid-btn");
 const warningText = document.querySelector("#warning");
 let squaresPerSide = 16;
 
-let maxWidth = 960;
+const MAX_WIDTH = 960;
+const MAX_RGB_VALUE = 256
+
+function randomColorChannel(){
+    return Math.floor(Math.random() * MAX_RGB_VALUE);
+}
+
+function randomRGB(){
+    const r = randomColorChannel()
+    const g = randomColorChannel()
+    const b = randomColorChannel()
+    return `rgb(${r}, ${g}, ${b})`
+}
 
 function makeTheGrid(rows, cols) {
-    let cellWidth = maxWidth / rows;
+    let cellWidth = MAX_WIDTH / rows;
     for (let i = 0; i < rows; ++i){
         const gridRow = document.createElement('div');
         for (let j = 0; j < cols; ++j){
@@ -19,7 +31,20 @@ function makeTheGrid(rows, cols) {
         }
         gridContainer.appendChild(gridRow);
     }
-    
+    gridContainer.addEventListener("mouseover", function(e){
+        const target = e.target;
+        if (target.tagName === "DIV") {
+            target.style.backgroundColor = randomRGB();
+        }
+    })
+
+    gridContainer.addEventListener("mouseout", function(e){
+        function setDelay(){
+            e.target.style.backgroundColor = "#fff";
+        }
+
+        setTimeout(setDelay, 250);
+    })
 }
 
 
